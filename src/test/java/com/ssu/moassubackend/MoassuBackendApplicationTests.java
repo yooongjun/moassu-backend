a2a70ac82d71c468f4e1928fb62a92c7e1efa01e
JUN <as2686@naver.com>
2023-04-02 16:12:20 +0900

    프로젝트 생성

---
+++ src/test/java/com/ssu/moassubackend/MoassuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.moassubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoassuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

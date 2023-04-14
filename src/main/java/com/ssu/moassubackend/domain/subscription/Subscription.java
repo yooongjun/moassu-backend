9fb82eee779fcdb3b2b5707ad81101565ab77ecf
JUN <as2686@naver.com>
2023-04-14 19:31:23 +0900

    Create User, Department and Subscription Entity

---
+++ src/main/java/com/ssu/moassubackend/domain/subscription/Subscription.java
@@ -0,0 +1,31 @@
+package com.ssu.moassubackend.domain.subscription;
+
+import com.ssu.moassubackend.domain.department.Department;
+import com.ssu.moassubackend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @Builder
+    public Subscription(User user, Department department) {
+        this.user = user;
+        this.department = department;
+    }
+
+}

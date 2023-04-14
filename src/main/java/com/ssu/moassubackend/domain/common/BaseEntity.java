71410ed7e837b4bd1e09ba16160d7130dbc674b0
JUN <as2686@naver.com>
2023-04-14 17:31:35 +0900

    Add JPA Auditing feature

---
+++ src/main/java/com/ssu/moassubackend/domain/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.ssu.moassubackend.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
